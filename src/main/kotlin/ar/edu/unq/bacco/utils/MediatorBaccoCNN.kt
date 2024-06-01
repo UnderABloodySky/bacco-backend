000b79bf04048b76571e6e1f895eeef41917a8ec
Federico Cameriere <fede.cameriere@gmail.com>
2024-05-31 21:44:21 -0300

    fix file upload, adding default values for application properties

--- src/main/kotlin/ar/edu/unq/bacco/utils/MediatorBaccoCNN.kt
+++ src/main/kotlin/ar/edu/unq/bacco/utils/MediatorBaccoCNN.kt
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.HttpClients
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.nio.file.Files
@@ -17,14 +18,11 @@ import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import org.springframework.http.ResponseEntity as ResponseEntity1
 
-class MediatorBaccoCNN {
-    @Value("\${django.backend.url}")
-    private lateinit var djangoBackendUrl: String
-
-    @Value("\${file.path.photos}")
-    private lateinit var filePath: String
-
-
+@Component
+class MediatorBaccoCNN(
+    @Value("\${file.path.photos}") private var filePath: String = "D:/UNQUI/TIP/bacco-backend/uploads/",
+    @Value("\${django.backend.url}") private var djangoBackendUrl: String = "http://localhost:8000/upload-photo",
+) {
     fun detectBeverage(file: MultipartFile): ResponseEntity<String> {
         val fileName = "${System.currentTimeMillis()}_${file.originalFilename}"
         val uploadDir = File("uploads")
