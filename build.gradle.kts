bb64444c3f2cba8c1554ebe092c67c094517e305
UnderABloodySky <valenzuelahoracioe@gmail.com>
2024-06-01 00:22:05 -0300

    Add application-test.properties and dependencies for H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ configurations {
 */
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
 	implementation("org.apache.httpcomponents:httpclient:4.5.13")
 	implementation("org.apache.httpcomponents:httpmime:4.5.13")
 	implementation("org.springframework.boot:spring-boot-starter-web")
--- src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
+++ src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.annotation.Transactional
