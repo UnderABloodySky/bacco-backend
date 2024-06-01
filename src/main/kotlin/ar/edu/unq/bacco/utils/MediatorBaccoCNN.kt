a29c752d0779e1c1f54fc8b676b0825e52562be7
UnderABloodySky <valenzuelahoracioe@gmail.com>
2024-05-31 21:08:03 -0300

    Add annotation

--- src/main/kotlin/ar/edu/unq/bacco/utils/MediatorBaccoCNN.kt
+++ src/main/kotlin/ar/edu/unq/bacco/utils/MediatorBaccoCNN.kt
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.HttpClients
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.nio.file.Files
@@ -17,6 +18,7 @@ import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import org.springframework.http.ResponseEntity as ResponseEntity1
 
+@Component
 class MediatorBaccoCNN {
     @Value("\${django.backend.url}")
     private lateinit var djangoBackendUrl: String
