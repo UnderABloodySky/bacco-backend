a283fa5a1a9f8a7c0fc1b6626ce259b77d14f641
UnderABloodySky <valenzuelahoracioe@gmail.com>
2024-06-01 06:59:53 -0300

    Tests working  but in the production DB

--- src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
+++ src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
@@ -6,28 +6,15 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.http.HttpStatus
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-/*
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 class UserControllerTest {
-
-    @Autowired
-    lateinit var userService: UserService
-
-    @Autowired
-    lateinit var userController: UserController
 
     @Test
     fun testCreateMockUser() {
         val userServiceMock = mock(UserService::class.java)
         val userController = UserController(userServiceMock)
-        val anUser = User(name="John Coltrane")
+        val anUser = User(name = "John Coltrane")
 
         `when`(userServiceMock.save(anUser)).thenReturn(anUser)
 
@@ -41,7 +28,7 @@ class UserControllerTest {
     fun testGetMockUserById() {
         val userServiceMock = mock(UserService::class.java)
         val userController = UserController(userServiceMock)
-        val user = User(name="Louis Amstrong")
+        val user = User(name = "Louis Amstrong")
 
         `when`(userServiceMock.findById(1L)).thenReturn(user)
 
@@ -50,28 +37,4 @@ class UserControllerTest {
         assertEquals(HttpStatus.OK, response.statusCode)
         assertEquals(user, response.body)
     }
-
-    @Test
-    fun `test createUser`() {
-        val anUser = User(name = "Nina Simone")
-
-        val response = userController.createUser(anUser)
-
-        assertEquals(HttpStatus.CREATED, response.statusCode)
-        assertEquals(anUser, response.body)
-    }
-
-    @Test
-    fun `test getUserById`() {
-        val user = User(name = "Buddy Rich")
-        val savedUser = userService.save(user)
-
-        val response = userController.getUserById(savedUser.id!!)
-
-        assertEquals(HttpStatus.OK, response.statusCode)
-        assertEquals(user, response.body)
-        assertEquals(user, savedUser)
-
-    }
 }
-*/
