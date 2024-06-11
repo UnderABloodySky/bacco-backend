0bafbb7dfec32c13d98ba02be64f3252d0cbec44
UnderABloodySky <valenzuelahoracioe@gmail.com>
2024-06-11 02:58:54 -0300

    Add complete tests about usercontroller

--- src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
+++ src/test/kotlin/ar/edu/unq/bacco/controller/UserControllerTest.kt
@@ -3,16 +3,20 @@ package ar.edu.unq.bacco.controller
 import ar.edu.unq.bacco.model.User
 import ar.edu.unq.bacco.service.RecipeService
 import ar.edu.unq.bacco.service.UserService
+import org.apache.http.HttpStatus
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatusCode
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.annotation.Transactional
+import kotlin.properties.Delegates
 
 /*
 import ar.edu.unq.bacco.model.User
@@ -39,6 +43,7 @@ package ar.edu.unq.bacco.controller
 @ExtendWith(SpringExtension::class)
 @Transactional
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DisplayName("UserController - Test: ")
 class UserControllerTest {
 
     private lateinit var userController: UserController
@@ -48,80 +53,51 @@ class UserControllerTest {
 
     @Autowired
     private lateinit var recipeService: RecipeService
+    private lateinit var userNoPersist: User
+    private lateinit var userPersist: User
 
     @BeforeEach
     fun setUp() {
         userController = UserController(userService, recipeService)
+        userNoPersist = User(name = "Louis Amstrong")
+        userPersist = userService.save(userNoPersist)
+
     }
 
     @Test
-    @Transactional
-    fun testCreateUserWithoutMock() {
-        val userNoPersist = User(name = "Pepe")
+    @DisplayName("Create an user returns a Response with 200 code")
+    fun testCreateUserReturnsAResponseWith200Code() {
         val response = userController.createUser(userNoPersist)
-        val user = response.body
-        val userPersist = userController.getUserById(user?.id!!).body
-        assertEquals(userNoPersist.name, userPersist?.name)
+        assertEquals(201, response.statusCodeValue)
+    }
+
+    @Test
+    @DisplayName("Create an user returns a status")
+    fun testCreateUserReturnsAStatus() {
+        val response = userController.createUser(userNoPersist)
+        assertEquals(HttpStatusCode.valueOf(201), response.statusCode)
+    }
+
+
+    @Test
+    @DisplayName("Find a user by ID returns a correct user when the ID is right")
+    fun testFindByUserIdWithCorrectUserReturnAResponseWith200Code() {
+        val response = userController.getUserById(userPersist.id!!)
+        assertEquals(200, response.statusCodeValue)
+    }
+
+    @Test
+    @DisplayName("Find by user Id with correct user return a response with 200 status code")
+    fun testFindByUserIdWithCorrectUserReturnAResponseWith200status() {
+        val response = userController.getUserById(userPersist.id!!)
+        assertEquals(HttpStatusCode.valueOf(200), response.statusCode)
+    }
+
+    @Test
+    @DisplayName("Find a user by ID returns a correct user when the ID is right")
+    fun testFindByUserIdReturnsCorrectUser() {
+        val user = userController.getUserById(userPersist.id!!).body
+        assertEquals(user?.id, userPersist.id)
+        assertEquals(user?.name, userPersist.name)
     }
 }
-
-/*
-@TestPropertySource(locations = ["classpath:application-test.properties"])
-@ActiveProfiles("test")
-class UserControllerTest {
-
-    private lateinit var userController: UserController
-
-    @Autowired
-    private lateinit var userService: UserService
-
-    @Autowired
-    private lateinit var recipeService: RecipeService
-
-
-    @BeforeEach
-    fun setUp(){
-        userController = UserController(userService, recipeService)
-    }
-
-    @Test
-    fun testCreateMockUser() {
-        val userServiceMock = mock(UserService::class.java)
-        val recipeServiceMock = mock(RecipeService::class.java)
-        val userController = UserController(userServiceMock, recipeServiceMock)
-        val anUser = User(name = "John Coltrane")
-
-        `when`(userServiceMock.save(anUser)).thenReturn(anUser)
-
-        val response = userController.createUser(anUser)
-
-        assertEquals(HttpStatus.CREATED, response.statusCode)
-        assertEquals(anUser, response.body)
-    }
-
-    @Test
-    fun testGetMockUserById() {
-        val userServiceMock = mock(UserService::class.java)
-        val recipeServiceMock = mock(RecipeService::class.java)
-        val userController = UserController(userServiceMock, recipeServiceMock)
-        val user = User(name = "Louis Amstrong")
-
-        `when`(userServiceMock.findById(1L)).thenReturn(user)
-
-        val response = userController.getUserById(1L)
-
-        assertEquals(HttpStatus.OK, response.statusCode)
-        assertEquals(user, response.body)
-    }
-
-    @Test
-    fun testCreateUserWithoutMock(){
-        val userNoPersist = User(name = "Pepe")
-        val response = userController.createUser(userNoPersist)
-        val user = response.body
-        val userPersist = userController.getUserById(user?.id!!).body
-        assertEquals(userNoPersist.name, userPersist?.name)
-
-    }
-}
-*/
